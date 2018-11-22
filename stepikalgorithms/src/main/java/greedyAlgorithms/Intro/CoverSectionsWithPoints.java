28560b5173ac9719d6757829552702e0c15c6694
Eteri <Eteri.Dzhibladze@sidenis.com>
2018-11-22 17:52:34 +0300

    non-compiled preparation

--- stepikalgorithms/src/main/java/greedyAlgorithms/Intro/CoverSectionsWithPoints.java
+++ stepikalgorithms/src/main/java/greedyAlgorithms/Intro/CoverSectionsWithPoints.java
@@ -1,9 +1,29 @@
 package greedyAlgorithms.Intro;
 
+import java.math.BigInteger;
+import java.util.*;
+
 public class CoverSectionsWithPoints {
 
+    static ArrayList<BigInteger> numberOfPointsInSections(BigInteger[][] sections) {
+        ArrayList<BigInteger> points = new ArrayList<BigInteger>();
+        return points;
+    }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        BigInteger[][] sections;
+        if (n >= 1 && n <= 100) {
+            for (int i = 0; i < n; i++) {
+                BigInteger start = scanner.nextBigInteger();
+                BigInteger finish = scanner.nextBigInteger();
+                if (start.compareTo(BigInteger.ZERO) > 0 && finish.compareTo(start) > 0) {
+                }
+            }
+        }
+        ArrayList<BigInteger> points = numberOfPointsInSections(sections);
+        System.out.println(points.size());
+        System.out.println(points);
     }
 }
